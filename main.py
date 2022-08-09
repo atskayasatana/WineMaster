a4ab3f010c4e4b9672863bf603a579e949341893
Anna <88509622+atskayasatana@users.noreply.github.com>
2022-08-09 23:35:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,56 +7,58 @@ import re
 import pandas as pd
 import pprint
 from collections import defaultdict
+import argparse
+import os
+
+WINERY_FOUNDATION_YEAR = 1920
 
 
-def end_form(number):
-    end_form = ''
-    num_string =str(number)
-    num_string_len = len(num_string)
-    if num_string_len < 2:
-        if int(num_string) in [0, 5, 6, 7, 8, 9]:
-            end_form = 'лет'
-        elif int(num_string) in [2, 3, 4]:
-            end_form = 'года'
-        elif int(num_string) == 1:
-            end_form = 'год'
+def add_correct_suffix(number):
+    suffix = ''
+    number = number % 100
+    if number > 19:
+        number = number % 10
+    if number == 1:
+        suffix = 'год'
+    elif 2 <= number <= 4:
+        suffix = 'года'
     else:
-        end_numbers = num_string[-2:]
-        if re.match(r'^[^1]1{1}$', end_numbers):
-            end_form = "год"
-        if re.match(r'^[^1][2-4]{1}$', end_numbers):
-            end_form = "года"   
-        if re.match(r'^1{1}[0-9]{1}$', end_numbers):
-            end_form = "лет"
-        if re.match(r'^[^1][5-9]{1}$''', end_numbers):
-            end_form = "лет"
-    return end_form        
+        suffix = 'лет' 
+    return suffix        
     
+
+if __name__ == '__main__':
     
-wine_df = pd.read_excel('wine.xlsx', na_values='').fillna('')
-wine_categories = list(set(wine_df['Категория']))
+    default_path = os.path.abspath(os.curdir)+'\wine.xlsx'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', nargs='?', default=default_path)    
+    user_path = parser.parse_args().path
+    wine_df = pd.read_excel(user_path, na_values='').fillna('')
+    wine_categories = list(set(wine_df['Категория']))
 
-wine_dict = defaultdict(list, [])
+    beverages = defaultdict(list, [])
 
-columns_for_value = ['Название', 'Сорт', 'Цена', 'Картинка', 'Акция']
-for i in range(len(wine_df)):
-    wine_dict[wine_df.loc[i, 'Категория']]\
-        .append(wine_df.loc[i, columns_for_value])
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html'])
-)
-
-template = env.get_template('template.html')
-rendered_page = template.render(
-    age=datetime.datetime.now().year-1920,
-    form=end_form(datetime.datetime.now().year-1920),
-    num_of_wine_categories=len(wine_categories),
-    wine_categories=wine_categories,
-    wine_dict=wine_dict 
+    beverage_description = [column for column in wine_df.columns
+                            if column != 'Категория']
+    
+    for row in range(len(wine_df)):
+        beverages[wine_df.loc[row, 'Категория']]\
+            .append(wine_df.loc[row, beverage_description])
+        
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html'])
     )
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+
+    template = env.get_template('template.html')
+    rendered_page = template.render(
+        age=datetime.datetime.now().year-WINERY_FOUNDATION_YEAR,
+        form=add_correct_suffix(datetime.datetime.now().year -
+                                WINERY_FOUNDATION_YEAR),
+        beverages=beverages
+        )
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
 
