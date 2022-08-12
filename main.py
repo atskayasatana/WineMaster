75ee1bfe288e37feda5858a9a6b1ad40595fc59c
Anna <88509622+atskayasatana@users.noreply.github.com>
2022-08-13 00:46:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-from jinja2 import Template
-from jinja2 import Environment, FileSystemLoader
-from jinja2 import PackageLoader, select_autoescape
-import datetime
-import re
-import pandas as pd
-import pprint
-from collections import defaultdict
 import argparse
+import datetime
+import pandas as pd
 import os
+
+from collections import defaultdict
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from pathlib import Path
 
 WINERY_FOUNDATION_YEAR = 1920
 
 
-def add_correct_suffix(number):
+def correct_noun(number):
     suffix = ''
     number = number % 100
     if number > 19:
@@ -29,21 +27,15 @@ def add_correct_suffix(number):
 
 if __name__ == '__main__':
     
-    default_path = os.path.abspath(os.curdir)+'\wine.xlsx'
+    default_path = Path.cwd().joinpath('wine.xlsx')
     parser = argparse.ArgumentParser()
     parser.add_argument('path', nargs='?', default=default_path)    
     user_path = parser.parse_args().path
-    wine_df = pd.read_excel(user_path, na_values='').fillna('')
-    wine_categories = list(set(wine_df['Категория']))
-
-    beverages = defaultdict(list, [])
-
-    beverage_description = [column for column in wine_df.columns
-                            if column != 'Категория']
-    
-    for row in range(len(wine_df)):
-        beverages[wine_df.loc[row, 'Категория']]\
-            .append(wine_df.loc[row, beverage_description])
+    wine_df = pd.read_excel(user_path, na_values=None, keep_default_na=False)\
+                .to_dict(orient='records')
+    beverages = defaultdict(list)
+    for wine in wine_df:
+        beverages[wine['Категория']].append(wine)
         
     env = Environment(
         loader=FileSystemLoader('.'),
@@ -53,12 +45,11 @@ if __name__ == '__main__':
     template = env.get_template('template.html')
     rendered_page = template.render(
         age=datetime.datetime.now().year-WINERY_FOUNDATION_YEAR,
-        form=add_correct_suffix(datetime.datetime.now().year -
-                                WINERY_FOUNDATION_YEAR),
+        form=correct_noun(datetime.datetime.now().year -
+                          WINERY_FOUNDATION_YEAR),
         beverages=beverages
         )
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
     server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
     server.serve_forever()
-
